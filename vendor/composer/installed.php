96f56f4bf7675b8b9c30f8d7a2ef62fad7f8fd8a
srtfisher <srtfisher@users.noreply.github.com>
2026-04-20 15:37:50 +0000

    Built changes from develop 0b0c5beda997945a3d4882355c4142b1061684b5

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/wp-new-relic-transactions',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '0b0c5beda997945a3d4882355c4142b1061684b5',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/wp-new-relic-transactions' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '0b0c5beda997945a3d4882355c4142b1061684b5',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
